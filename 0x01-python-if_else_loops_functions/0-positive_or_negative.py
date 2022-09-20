3bf3553dc2138a20f9b7f8d2f2e6503a7cd2e1ec
Christian joshua <82231891+thelastmedici@users.noreply.github.com>
2022-09-20 20:00:16 +0100

    Delete 0x01-python-if_else_loops_functions directory
    i deleted it

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-    if number < 0:
-print(number, "is negative")
-    if number == 0:
-print(number, "is zero")
-    if number > 0:
-print(number, "is positive")
