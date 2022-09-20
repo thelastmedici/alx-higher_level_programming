42ddf28d6f8d082038778ae12993ed142894f654
thelastmedici <christianopeyemijoshua@gmail.com>
2022-09-20 02:31:15 -0700

    python

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-5, 5)
+if number < 0;
+print(number, "is negative")
+if number == 0:
+print(number, "is zero")
+if number > 0;
+print(number, "is positive")
+
