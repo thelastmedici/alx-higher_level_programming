77e1545d3a1de4df01d5525538bcccb8e600212b
thelastmedici <christianopeyemijoshua@gmail.com>
2022-10-16 00:56:32 +0000

    function and loop

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for character in range(97, 123):
-    if (character != 101 and character != 113):
-        print("{:c}".format(character), end='')
+    for char in range(ord("a"), ord("z") + 1):
+        print("{:c}".format(char), end='')
