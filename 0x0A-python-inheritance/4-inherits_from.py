acfa43e235eabb690aae01be0bf4aa98a16d033a
user.name: "thelastmedici <user.email: "christianopeyemijoshua@gmail.com>
2024-02-24 02:49:30 -0500

    added file 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""checks if object is an instance of a class that
-inherited from the specified class or not
-"""
+""" check if inherited """
 
 
 def inherits_from(obj, a_class):
-    """Returns true if object is an instance of a class that inherited
-    (directly or indirectly) from the specified class; otherwise False
-    """
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+    """ subclass find """
+    return isinstance(obj, a_class) and not type(obj) is a_class
