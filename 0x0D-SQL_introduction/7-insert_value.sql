e9acd73dc90a29dcda1d289953ffdfaf8ee502c8
user.name: "thelastmedici <user.email: "christianopeyemijoshua@gmail.com>
2024-03-26 18:07:08 -0400

    added solution to task 8

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- insert a new row into the table in your msql server
 -- The database name will be passed as an argument of the mysql command
 
-INSERT INTO hbtn_0c_0(id, name,) VALUES (89, "Best School")
+INSERT INTO first_table(id, name,) VALUES (89, "Best School")
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- displays the number of records with a particular id
+-- database name will be passed as an argument of the mysql command
+
+SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
