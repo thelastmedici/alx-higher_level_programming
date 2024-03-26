e04ee4b8a74f09f5f32910f90e09471d96765859
user.name: "thelastmedici <user.email: "christianopeyemijoshua@gmail.com>
2024-03-26 18:29:08 -0400

    added solution to task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- select all rows from the table second_table in the database hbtn_0c_0
+-- select all rows from the table
+
+SELECT * FROM hbtn_0c_0.second_table;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,5 @@
+-- create table second_table in the database hbtn_0c_0
+-- The table second_table should have the same columns as first_table
+-- The id column should be a primary key
+
+CREATE IF NOT EXISTS hbtn_0c_0.second_table (id INT, name VARCHAR(256), score INT);
